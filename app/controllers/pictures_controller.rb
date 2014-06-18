224e8ed3de25c5da1604f027a2007161ae135df6
Drew Martin <drew.e.martin@gmail.com>
2014-06-18 14:55:27 -0400

    Routes>Controller>view>Repeat

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -40,27 +40,11 @@ class PicturesController < ApplicationController
     @picture = @pictures[params[:id].to_i]
   end
 
-  # def picture0
-  #   @picture = {
-  #     :title  => "The old church on the coast of White sea",
-  #     :artist => "Sergey Ershov",
-  #     :url    => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
-  #   }
-  # end
+  def new
 
-  # def picture1
-  #   @picture = {
-  #     :title  => "Sea Power",
-  #     :artist => "Stephen Scullion",
-  #     :url    => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
-  #   }
-  # end
+  end
 
-  # def picture2
-  #   @picture = {
-  #     :title  => "Into the Poppies",
-  #     :artist => "John Wilhelm",
-  #     :url    => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
-  #   }
-  # end
+  def create
+
+  end
 end
