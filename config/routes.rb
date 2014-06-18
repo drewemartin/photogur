7fe53865de9750a83e347e0819b39e51bbd09447
Drew Martin <drew.e.martin@gmail.com>
2014-06-18 16:33:09 -0400

    assigned root as pictures#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Photogur::Application.routes.draw do
 
+root :to => 'pictures#index'
+#this makes the index the homepage/root instead
+# of the "Rails" page
+
 get 'pictures' => 'pictures#index'
 
 get 'pictures/new' => 'pictures#new'
