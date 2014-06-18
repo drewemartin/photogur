353f5edbb3a13c8256ca96b87a9fcec214f7c2d8
Drew Martin <drew.e.martin@gmail.com>
2014-06-18 17:51:03 -0400

    added edit feature

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,11 @@ root :to => 'pictures#index'
 
 get 'pictures' => 'pictures#index'
 
-get 'pictures/new' => 'pictures#new'
-post 'pictures/' => 'pictures#create'
+get 'pictures/new' => 'pictures#new' #add new part 1
+post 'pictures/' => 'pictures#create' #add new part 2
+
+get 'pictures/:id/edit' => 'pictures#edit', as: "edit_picture" #edit part 1
+patch 'pictures/:id' => 'pictures#update' #edit part 2
  
 
 get 'pictures/:id' => 'pictures#show', as: "picture"
