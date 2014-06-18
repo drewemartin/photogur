2e18ed70bb3b1a6e5a7e071802914620b9c54294
Drew Martin <drew.e.martin@gmail.com>
2014-06-18 16:29:50 -0400

    refactored form

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,19 +1,31 @@
 class PicturesController < ApplicationController
 	def index
 		@pictures = Picture.all
+    # Picture refers to the Picture table under the model
+    # @picture(s) is plural since it referes to all pictures 
+    # held in the DB. plus since it contains multiple items
+    # it is iterated over on index.html.erb 
 	end
 
   def show
-    @pictures = Picture.find(params[:id].to_i)
+    @picture = Picture.find(params[:id].to_i)
   end
 
   def new
-
+      @picture = Picture.new
   end
 
   def create
-    render :text => "Saving a picture. URL #{params[:url]}, Title: #{params[:title]}, Artist #{params[:artist]}"
-    #the symbol ":text" can be rendered since 
-    #the form fields were set as text fields 
+    @picture = Picture.new(picture_params)
+      if @picture.save
+        redirect_to pictures_url
+      else
+        render :new #(:new refers to new.html.erb)
+      end
+  end
+
+  private
+  def picture_params
+    params.require(:picture).permit(:artist, :title, :url)
   end
 end
